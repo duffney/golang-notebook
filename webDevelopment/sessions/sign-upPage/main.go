7fdec9d2eb59f6cb81c896f4e7490541f4e5a1c3
Josh Duffney <josh@duffney.io>
2022-06-14 12:02:50 -0500

    added basic signup web app sample

--- webDevelopment/sessions/sign-upPage/main.go
+++ webDevelopment/sessions/sign-upPage/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os/exec"
+	"strings"
 	"text/template"
 )
 
 type user struct {
 	UserName string
 	Password string
-	First    string
-	Last     string
 }
 
 var tpl *template.Template
@@ -28,9 +30,58 @@ func main() {
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	u := getUser(req)
+	tpl.ExecuteTemplate(w, "index.gohtml", u)
 }
 
 func signup(w http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodPost {
+		u := req.FormValue("username")
+		p := req.FormValue("password")
+
+		// does the user already exists?
+		if _, ok := users[u]; ok {
+			http.Error(w, "Username already taken", http.StatusForbidden)
+			return
+		}
+
+		// create a session (map UUID to User)
+		sessionUUID, err := exec.Command("uuidgen").Output()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: strings.TrimSuffix(string(sessionUUID), "\n"),
+		}
+		http.SetCookie(w, cookie)
+		sessions[cookie.Value] = u
+
+		// add user to users database
+		newUser := user{u, p}
+		users[u] = newUser
+
+		// redirect to index
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		fmt.Println(users)
+		return
+	}
+
 	tpl.ExecuteTemplate(w, "signup.gohtml", nil)
+
+}
+
+func getUser(req *http.Request) user {
+	var u user
+
+	c, err := req.Cookie("session")
+	if err != nil {
+		return u
+	}
+
+	if un, ok := sessions[c.Value]; ok {
+		u = users[un]
+	}
+	return u
 }
