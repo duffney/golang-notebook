7593090ce56f07e699c9c0f9f6edbc8c9da9acae
Joshua Duffney <jduffney@microsoft.com>
2022-12-07 07:28:13 -0600

    LeetCode - Merge Two Sorted Lists

--- leetcode/Merge Two Sorted Lists/main.go
+++ leetcode/Merge Two Sorted Lists/main.go
@@ -7,37 +7,64 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func main() {
-	l1 := NewNode(1)
-	l1.Next = NewNode(2)
-	l1.Next.Next = NewNode(4)
-
-	l2 := NewNode(1)
-	l2.Next = NewNode(3)
-	l2.Next.Next = NewNode(4)
-	//TraverseLinkedList(l1)
-	fmt.Println(mergeTwoLists(l1, l2))
-}
-
-func NewNode(value int) *ListNode {
-	var n ListNode
-	n.Val = value
-	n.Next = nil
-	return &n
-}
-
-func TraverseLinkedList(head *ListNode) {
-	fmt.Printf("Linked List: ")
-	temp := head
-	for temp != nil {
-		fmt.Printf("%d ", temp.Val)
-		temp = temp.Next
+func NewLinkedList(val int, others ...int) *ListNode {
+	ll := &ListNode{Val: val}
+	current := ll
+	for _, other := range others {
+		current.Next = &ListNode{Val: other}
+		current = current.Next
 	}
+	return ll
+}
+
+func (ll *ListNode) toArray() []int {
+	vals := []int{}
+	current := ll
+	for current != nil {
+		vals = append(vals, current.Val)
+		current = current.Next
+	}
+	return vals
 }
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	if list1 == nil && list2 == nil {
-		return nil
+	h := &ListNode{} // dummy head with nul to avoid having to create a head value
+	l := h
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			l.Next = list1
+			list1 = list1.Next
+		} else {
+			l.Next = list2
+			list2 = list2.Next
+		}
+		l = l.Next // create next node in Linked List
 	}
-	return list2
+
+	if list1 == nil {
+		l.Next = list2 // append the remaining list2 nodes
+	}
+
+	if list2 == nil {
+		l.Next = list1 // append the remaining list1 nodes
+	}
+
+	return h.Next
+}
+
+func (ll *ListNode) ToArray() []int {
+	vals := []int{}
+	current := ll
+	for current != nil {
+		vals = append(vals, current.Val)
+		current = current.Next
+	}
+	return vals
+}
+
+func main() {
+	list1 := NewLinkedList(1, 2, 4)
+	list2 := NewLinkedList(1, 3, 4)
+	output := mergeTwoLists(list1, list2)
+	fmt.Println(output.toArray())
 }
